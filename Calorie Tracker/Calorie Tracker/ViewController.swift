6fa5d804554c0b9cd14139fb51a772bffa59004e
Enayatullah Naseri <enayatullahna@gmail.com>
2020-02-25 18:17:09 -0800

    Started the Calorie Tracker app

---
+++ Calorie Tracker/Calorie Tracker/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Calorie Tracker
+//
+//  Created by Enayatullah Naseri on 2/25/20.
+//  Copyright © 2020 Enayatullah Naseri. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
